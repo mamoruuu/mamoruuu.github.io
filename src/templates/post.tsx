bb01d02ac8347dd7d44b00464172072f0f4832b9
Alexander Bolotskov <r3disrussian@gmail.com>
2020-11-04 23:58:04 +0800

    feat: 🎸 add 'back' link to post page

--- src/templates/post.tsx
+++ src/templates/post.tsx
@@ -1,12 +1,30 @@
 import React from "react"
-import { graphql } from "gatsby"
+import { graphql, Link } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import { Header, PostContainer, UnderTitle, Title1 } from "../components/post-styles"
+import styled from "styled-components"
 
 interface Props {
   data: any
 }
+
+const Links = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  margin-bottom: 30px;
+`
+
+const ArrowIcon = styled.img`
+  display: inline-block;
+  width: 1.5em;
+  height: 1.5em;
+  line-height: 1;
+  margin: 0;
+  margin-right: 10px;
+  vertical-align: middle;
+`
 
 const PostTemplate: React.FC<Props> = ({ data }) => {
   const { markdownRemark } = data
@@ -16,6 +34,12 @@ const PostTemplate: React.FC<Props> = ({ data }) => {
     <Layout>
       <SEO title={post.title} />
       <PostContainer>
+        <Links>
+          <Link to="/">
+            <ArrowIcon src={require('../images/icons/left-arrow.svg')} alt=""/>
+            Back to blog
+          </Link>
+        </Links>
         <article>
           <Header>
             <Title1>{post.title}</Title1>
