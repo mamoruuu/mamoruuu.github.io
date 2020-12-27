ac86f15cf5f1d3bb4a3395f69d1e05a460f3dc0f
Alexander Bolotskov <r3disrussian@gmail.com>
2020-12-27 22:27:14 +0800

    fix: 🐛 removes trees, buildings and mountains
    They look ugly

--- src/components/xmas-installation.tsx
+++ src/components/xmas-installation.tsx
@@ -1,51 +1,6 @@
 import React from "react"
 import styled, { createGlobalStyle, keyframes } from "styled-components"
 
-const Trees = styled.div`
-  width:100%;
-  position: absolute;
-  height: 300px;
-  z-index: -2;
-  background-image: url(${require('../images/trees.svg')});
-  background-repeat: repeat-x;
-  background-position: bottom;
-  opacity: 1;
-  bottom: 0;
-  left: 0;
-  right: 0;
-
-  @media (max-width: 560px) {
-    height: 240px;
-    background-size: 540px;
-  }
-`
-
-const Mountains = styled.img`
-  position: absolute;
-  z-index: -3;
-  left: 0;
-  bottom: 0;
-  opacity: .6;
-  width: 250px;
-  transform: scaleX(-1);
-
-  @media (max-width: 560px) {
-    width: 210px;
-  }
-`
-
-const Town = styled.img`
-  position: absolute;
-  z-index: -1;
-  right: 0;
-  bottom: -25px;
-  width: 400px;
-
-  @media (max-width: 560px) {
-    display: none;
-  }
-`
-
 const snowFall = keyframes`
   from {
     background-position: 0 0;
@@ -88,9 +43,6 @@ const GlobalStyleOverride = createGlobalStyle`
 
 const XmasInstallation = () => <Installation>
   <GlobalStyleOverride />
-  <Trees />
-  <Mountains src={require('../images/mountain.svg')} />
-  <Town src={require('../images/town2.svg')} />
 </Installation>
 
 export default XmasInstallation
