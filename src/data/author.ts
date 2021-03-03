a754b28aeb7b6cc04ce7511881b1d6e9cedb67f0
Alexander Bolotskov <r3disrussian@gmail.com>
2021-03-04 00:43:26 +0800

    fix: 🐛 stops loving js, pizza and anime profile pictures

--- src/data/author.ts
+++ src/data/author.ts
@@ -4,6 +4,6 @@ export const fullName = "Alexander Bolotskov"
 
 const age = differenceInYears(new Date(), new Date('1997-03-30'));
 
-export const summary = `${age} y/o. He/him. Frontend developer\n(mostly, i also do some non-frontend thingies)\nI love JS, pizza and anime profile pictures`;
+export const summary = `${age} y/o. He/him. Frontend developer\n(mostly, i also do some non-frontend thingies)\nI like JS, pizza and anime profile pictures`;
 export const twitter = "@mamorukuuun";
 export const github = "@mamoruuu";
